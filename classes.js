7abf382d521400cca3297b9b5f459beb699091fe
bdurb <brian@rpmgreenville.com>
2018-05-24 14:11:35 -0700

    added method to child and incriments children and includes parent

--- classes.js
+++ classes.js
@@ -16,9 +16,26 @@ class Parent extends Person {
   constructor(props) {
     super(props);
     this.children = props.children;
+    this.children = props.children;
+    this.gchildren = props.gchildren;
   }
   hasChildren () {
     return `Hi,  i am ${this.name} and I have ${this.children} Children`
+  }
+};
+
+class Child extends Parent {
+  constructor(props) {
+    super(props);
+    this.favAnimal = props.favAnimal;
+    this.kid = props.kid;
+  }
+  animal () {
+    return `Hey, I'm ${this.name}, my fav animal is ${this.favAnimal}`
+  }
+  hadKid (parent) {
+    const amount = this.kid += 1;
+    return `${parent.name} has a new gchild, ${this.name} now has ${amount} children`
   }
 };
 
@@ -33,9 +50,21 @@ const joe = new Person({
 
 const albert = new Parent({
   "name": "Albert",
-  "children": 3
+  "children": 3,
+});
+
+const cindy = new Child({
+  "name": "Cindy",
+  "kid": 0
 });
 
 console.log(joe.speak());
 
 console.log(albert.hasChildren());
+
+console.log(cindy.hadKid(joe));
+console.log(cindy.hadKid(joe));
+console.log(cindy.hadKid(joe));
+console.log(cindy.hadKid(joe));
+console.log(cindy.hadKid(joe));
+console.log(cindy.hadKid(albert));
